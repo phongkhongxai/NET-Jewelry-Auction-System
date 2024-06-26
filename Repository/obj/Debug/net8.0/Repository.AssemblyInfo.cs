93ed24b6af66b269c9b2a6271004fc41acc71ccc
AnhKhoa14 <buikhoa2403@gmail.com>
2024-06-26 16:49:20 +0700

    update ui jewelry, create accept request

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125fdd2aa829cf424307c199d0d97d7a73f74811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8925a1d10358550a8531769c65dc2156335659a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
